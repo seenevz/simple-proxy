507aeffd0a854012bac5151fbfa096efc220c238
Sergio Neves <sernevess@gmail.com>
2020-11-26 21:43:15 +0000

    added full intermediary request with response headers baked in

---
+++ api/proxy.js
@@ -0,0 +1,23 @@
+module.exports = (req, resp) => {
+  const https = require("https");
+  const proxiedUrl = req.query.url;
+
+  https.get(proxiedUrl, originalResp => {
+    let bodyChunks = "";
+    const headers = {
+      ...originalResp.headers,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Expose-Headers": "*"
+    };
+
+    console.log(headers)
+
+    originalResp.on("data", chunck => (bodyChunks += chunck));
+    originalResp.on("end", () => {
+    
+    resp.writeHead(200, 'ok', headers)
+    resp.end(bodyChunks)
+    });
+  });
+
+};
