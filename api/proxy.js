97ec5cac9dd257e6eb8da4cc3167cec5d47c612f
Sergio Neves <sernevess@gmail.com>
2020-11-27 13:03:26 +0000

    refactor cors handler fn

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,28 @@
-module.exports = (req, resp) => {
+const urlProxy = (req, resp) => {
   const https = require("https");
+
   const {
     query: { url },
   } = req;
 
   https.get(url, originalResp => {
     let bodyChunks = "";
-    const headers = {
-      ...originalResp.headers,
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Expose-Headers": "*",
-    };
-
-    console.log(headers);
 
     originalResp.on("data", chunck => (bodyChunks += chunck));
+
     originalResp.on("end", () => {
       resp.writeHead(200, "ok", headers);
       resp.end(bodyChunks);
     });
   });
 };
+
+const corsHandler = fn => async (req, resp) => {
+  resp.setHeader("Access-Control-Allow-Origin", "*");
+  resp.setHeader("Access-Control-Expose-Headers", "*");
+  resp.setHeader("Access-Control-Allow-Methods", "GET");
+
+  return await fn(req, resp);
+};
+
+module.exports = corsHandler(urlProxy);
