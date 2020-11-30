f0d1f1e9650eba76739a2c670b7c1d2263109a13
Sergio Neves <sernevess@gmail.com>
2020-11-30 15:39:09 +0000

    added methods to create a new token

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,26 @@
 const { KJUR } = require("jsrsasign");
 
-const verifyToken = token => KJUR.jws.JWS.verifyJWT(token, process.env.SECRET);
+const allowedUsers = () => JSON.parse(process.env.ALLOWED_USERS);
+
+const verifyToken = token =>
+  KJUR.jws.JWS.verifyJWT(token, process.env.SECRET, {
+    alg: ["HS512"],
+    sub: allowedUsers(),
+  });
+
+const createToken = payload =>
+  KJUR.jws.JWS.sign(
+    "HS512",
+    { alg: "HS512", typ: "JWT" },
+    JSON.stringify(payload),
+    process.env.SECRET
+  );
+
+//   auth.createToken({
+//     sub: "seenevz",
+//   })
 
 module.exports = {
   verifyToken,
+  createToken,
 };
