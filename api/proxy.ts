ee95c6529f147387f5df7ee082d39198da0461b6
Sergio Neves <sernevess@gmail.com>
2022-05-17 13:05:33 +0100

    Changes to headers parsing

--- api/proxy.ts
+++ api/proxy.ts
@@ -6,7 +6,9 @@ import auth from "./_auth"
 
 type AsyncReqHandler = (req: VercelRequest, resp: VercelResponse) => Promise<void>
 
-const filterHeaders = (headers: IncomingHttpHeaders) => Object.entries(headers).filter((header) => header[0] === "content-type" || (header[0].includes("x-") && header[0] !== 'x-auth-token'))
+const headersRegex = /content-type|set-cookie|accept|x-\S*/i
+
+const filterHeaders = (headers: IncomingHttpHeaders) => Object.entries(headers).filter(header => !!headersRegex.exec(header[0]) && header[0] !== 'x-auth-token ')
 
 const addHeaders = (req: ClientRequest, headers: IncomingHttpHeaders) => filterHeaders(headers).forEach(([key, val]) => req.setHeader(key, val ?? ""))
 
@@ -28,6 +30,7 @@ const urlProxy = (req: VercelRequest, resp: VercelResponse) => {
         method,
       },
       proxyResp => {
+        if (proxyResp.headers["set-cookie"]) proxyResp.headers["set-cookie"].forEach((cookie, i, arr) => arr[i] = cookie.replace('samesite=lax', 'samesite=None; Secure'))
         resp.writeHead(proxyResp.statusCode || 200, {
           ...proxyResp.headers,
           ...resp.getHeaders(),
@@ -64,9 +67,11 @@ const urlProxy = (req: VercelRequest, resp: VercelResponse) => {
 
 
 const corsHandler = (fn: AsyncReqHandler) => async (req: VercelRequest, resp: VercelResponse) => {
-  resp.setHeader("Access-Control-Allow-Origin", "*");
-  resp.setHeader("Access-Control-Allow-Headers", "*");
+  // const { protocol, hostname, port } = new URL(req.headers.origin!)
+  resp.setHeader("Access-Control-Allow-Origin", req.headers.origin!);
+  resp.setHeader("Access-Control-Allow-Headers", "x-api-key");
   resp.setHeader("Access-Control-Expose-Headers", "*");
+  resp.setHeader("Access-Control-Allow-Credentials", "true")
   resp.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
 
   //To authenticate, request must have an header called x-auth-token with a valid token
